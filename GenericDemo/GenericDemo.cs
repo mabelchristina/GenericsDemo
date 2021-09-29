1b107e8426420685d850564d9bd1cc60d818c947
Mabel Christina <maybelchristina@gmail.com>
2021-09-29 12:51:55 +0530

    [Mabel] Add: UC1 added

---
+++ GenericDemo/GenericDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDemo
+{
+    class GenericDemo<T> where T:IComparable
+    {
+        public void MaximumOfNumber(int firstValue,int secondValue,int thirdValue)
+        {
+            if(firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue)>0||
+                firstValue.CompareTo(secondValue)>=0 && firstValue.CompareTo(thirdValue)>0||
+                firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue)>=0)
+                 {
+                     Console.WriteLine(firstValue);
+                 }
+            if(secondValue.CompareTo(firstValue)>0 && secondValue.CompareTo(thirdValue)>0||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0 )
+                {
+                    Console.WriteLine(secondValue);
+                }
+            if(thirdValue.CompareTo(firstValue)>0 && thirdValue.CompareTo(secondValue)>0 ||
+                    thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                Console.WriteLine(thirdValue);
+            }
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            GenericDemo<int> genericDemo = new GenericDemo<int>();
+            genericDemo.MaximumOfNumber(11, 5, 2);
+        }
+    }
+}
